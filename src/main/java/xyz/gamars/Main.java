53c716cd43545031baa841b8b0d560e1c9b31dbd
Daryan <trinhdaryan@gmail.com>
2024-02-18 19:40:22 -0800

    Added Kyronet & discord-game-sdk4j
    - Added Kyronet for our networking
    - Added discord-game-sdk4j to incorporate Discord Game SDK

--- src/main/java/xyz/gamars/Main.java
+++ src/main/java/xyz/gamars/Main.java
@@ -1,10 +1,17 @@
 package xyz.gamars;
 
+import de.jcm.discordgamesdk.Core;
+import de.jcm.discordgamesdk.CreateParams;
+import de.jcm.discordgamesdk.activity.Activity;
+
+import java.io.File;
+import java.time.Instant;
+
 public class Main {
 
     public static void main(String[] args) {
-        ThirteenGameText thirteenGameText = new ThirteenGameText();
-        thirteenGameText.run();
+
+
     }
 
 
